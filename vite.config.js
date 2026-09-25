2699d9ce1249430286ad020a5de3cfb9bd0b3d1f
Teng Joon Koo <rogerktj@gmail.com>
2026-09-25 13:03:11 +0800

    TJKOO 20260925: Fix Vercel deployment issues

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ export default defineConfig({
   plugins: [react()],
   // Change this to your repo name when deploying to GitHub Pages
   // e.g., base: '/weather-app/'
-  base: '/WeatherByRogerkoo',
+  base: '/'
 });
